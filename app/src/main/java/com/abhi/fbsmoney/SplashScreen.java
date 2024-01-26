086b282fedf5cf62c925e46f70e2277ecda35ef5
Abhi <avvjit@gmail.com>
2024-01-27 02:12:23 +0530

    SplashScreen implemented

---
+++ app/src/main/java/com/abhi/fbsmoney/SplashScreen.java
@@ -0,0 +1,45 @@
+package com.abhi.fbsmoney;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static int SPLASH_TIMER = 2000;
+
+    TextView textView;
+    Animation slideAnim, bottomAnim;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.splash_screen);
+
+        textView=findViewById(R.id.splash_text);
+
+        slideAnim= AnimationUtils.loadAnimation(this, R.anim.slide_anim);
+
+        textView.setAnimation(slideAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_TIMER);
+
+    }
+
+}
