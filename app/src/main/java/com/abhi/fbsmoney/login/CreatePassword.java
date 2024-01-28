694afac0b87090e785549b9457de2ce7570b4353
Abhi <avvjit@gmail.com>
2024-01-29 00:08:26 +0530

    Forget password implemented

--- app/src/main/java/com/abhi/fbsmoney/login/CreatePassword.java
+++ app/src/main/java/com/abhi/fbsmoney/login/CreatePassword.java
@@ -1,9 +1,19 @@
 package com.abhi.fbsmoney.login;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
 
+import com.abhi.fbsmoney.MainActivity;
 import com.abhi.fbsmoney.R;
 
 public class CreatePassword extends AppCompatActivity {
@@ -12,5 +22,41 @@ public class CreatePassword extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_password);
+    }
+
+    public void back12(View view) {
+
+        Intent intent = new Intent(getApplicationContext(), Otp.class);
+        startActivity(intent);
+
+    }
+
+    public void password(View view) {
+
+        showSheet();
+
+    }
+
+    private void showSheet() {
+
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.sheet_congrats);
+
+        AppCompatButton Continue2 = dialog.findViewById(R.id.Continue2);
+
+        Continue2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        dialog.show();
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.getWindow().getAttributes().windowAnimations = R.style.SheetAnimation;
+        dialog.getWindow().setGravity(Gravity.BOTTOM);
     }
 }
--- app/src/main/java/com/abhi/fbsmoney/login/Otp.java
+++ app/src/main/java/com/abhi/fbsmoney/login/Otp.java
@@ -1,13 +1,23 @@
 package com.abhi.fbsmoney.login;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.Toast;
 
 import com.abhi.fbsmoney.R;
+import com.abhi.fbsmoney.signup.FrontPhoto1;
+import com.abhi.fbsmoney.signup.Signup2;
+import com.abhi.fbsmoney.signup.VerifyScuss;
 
 public class Otp extends AppCompatActivity {
 
@@ -17,17 +27,40 @@ public class Otp extends AppCompatActivity {
         setContentView(R.layout.activity_otp);
     }
 
-    public void sendCode(View view) {
+    public void sendCode2(View view) {
 
-        Toast.makeText(this, "Next day works...", Toast.LENGTH_SHORT).show();
+        showSheet();
 
     }
 
-    public void back10(View view) {
+    public void back11(View view) {
 
         Intent intent = new Intent(getApplicationContext(), VerifyEmail.class);
         startActivity(intent);
 
+    }
+
+    private void showSheet() {
+
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.sheet_expired);
+
+        AppCompatButton resend = dialog.findViewById(R.id.resend);
+
+        resend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), CreatePassword.class);
+                startActivity(intent);
+            }
+        });
+
+        dialog.show();
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.getWindow().getAttributes().windowAnimations = R.style.SheetAnimation;
+        dialog.getWindow().setGravity(Gravity.BOTTOM);
     }
 
 }
